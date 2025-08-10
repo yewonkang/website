2e905b6df437f1bb23a56502058b451db38affc3
Yewon Kang <ywkang22@gmail.com>
2025-08-10 18:11:12 -0500

    fix

--- push.R
+++ push.R
@@ -1,7 +1,8 @@
 git remote add origin https://github.com/yewonkang/website.git
 git branch -M main
 git add --all
-git commit -m "change theme"
+git commit -m "fix"
 git push -u origin main
 
 quarto render && git add . && git commit -m "Update site" && git push origin main
+https://yewonkang.github.io/website/
