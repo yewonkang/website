327b80aeadf449173508b1438afa5aa61bf2d0b9
Yewon Kang <ywkang22@gmail.com>
2025-08-10 16:21:13 -0500

    add image

--- push.R
+++ push.R
@@ -1,5 +1,7 @@
 git remote add origin https://github.com/yewonkang/website.git
 git branch -M main
 git add --all
-git commit -m "change title"
+git commit -m "change theme"
 git push -u origin main
+
+quarto render && git add . && git commit -m "Update site" && git push origin main
